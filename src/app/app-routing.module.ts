3d4657b3541720e569d8fa2b9441c031ed2444d1
Burak Mert Muş <burakmertmus@gmail.com>
2021-08-30 22:11:34 +0300

    dashboard init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ArticleComponent } from './article/article.component';
+import { CommentsComponent } from './comments/comments.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PagesComponent } from './pages/pages.component';
+import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
   { path: '',   redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'pages', component: PagesComponent },
   { path: 'articles', component: ArticleComponent },
+  { path: 'comments', component: CommentsComponent },
+  { path: 'products', component: ProductsComponent },
   { path: '**', component: DashboardComponent }
 
   
